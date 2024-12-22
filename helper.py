e0598dbf5f3bd49c29130d0e7d9e2f3759b27a68
Kana <honkanenmarkus40@gmail.com.com>
2024-12-22 16:00:59 +0200

    helper avustajia

---
+++ helper.py
@@ -0,0 +1,11 @@
+﻿def hand_index_check(hand_index, player):
+    if hand_index < 0 or hand_index >= len(player.hands):
+        print(f"Invalid hand index for {player.name}. Player has {len(player.hands)} hands.")
+        return False
+    return True
+
+def money_check(bet_amount, player):
+    if bet_amount > player.saldo:
+        print("insufficient funds")
+        return False
+    return True
